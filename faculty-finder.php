e4dd246f4d81494936f966f0c00fe8e24fa0d93d
jamdlaw <jamdlaw@yahoo.com>
2025-08-07 12:56:24 -0700

    adding first file

---
+++ faculty-finder.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Plugin Name:       FacultyFinder
+ * Plugin URI:        https://github.com/your-username/faculty-finder
+ * Description:       A custom plugin to create, manage, and display a searchable staff directory.
+ * Version:           1.0.0
+ * Author:            Your Name
+ * Author URI:        https://your-portfolio-url.com
+ * License:           GPL-2.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       faculty-finder
+ *
+ * @package           Faculty_Finder
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+    die;
+}
