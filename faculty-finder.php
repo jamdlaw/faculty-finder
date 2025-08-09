1f55af50f7a7cea0533419e3e97f5d9fd8d3d50b
jamdlaw <jamdlaw@yahoo.com>
2025-08-09 15:42:46 -0700

    added first function

--- faculty-finder.php
+++ faculty-finder.php
@@ -15,3 +15,19 @@ if ( ! defined( 'WPINC' ) ) {
     die;
 }
 
+function ffinder_register_staff_cpt() {
+    $labels = [ /* ... Define labels like 'Staff', 'Add New Staff', etc. ... */ ];
+    $args = [
+        'label'               => __( 'Staff', 'faculty-finder' ),
+        'labels'              => $labels,
+        'supports'            => [ 'title', 'editor', 'thumbnail' ], // Default fields to use
+        'public'              => true,
+        'show_in_menu'        => true,
+        'menu_position'       => 5,
+        'menu_icon'           => 'dashicons-businessperson', // A fitting icon from Dashicons
+        'has_archive'         => true,
+        'rewrite'             => [ 'slug' => 'faculty' ],
+    ];
+    register_post_type( 'staff', $args );
+}
+add_action( 'init', 'ffinder_register_staff_cpt' );
